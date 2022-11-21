fcd6f153540998ecbbb6d943a3d9303cfaae8421
merchi92 <webma1132192@gmail.com>
2022-11-21 09:18:57 +0900

    font, color 추가

--- app/src/main/java/com/example/smartstore/ui/theme/Theme.kt
+++ app/src/main/java/com/example/smartstore/ui/theme/Theme.kt
@@ -7,24 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = CaffeLightWhite,
+    surface = CaffeMenuBack,
+    onSurface = CaffeMenuBack,
+    primary = CaffeBrownLight,
+    primaryVariant = CaffeBrown,
+    secondary = CaffeDarkBrown
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    background = CaffeLightWhite,
+    surface = CaffeMenuBack,
+    onSurface = CaffeMenuBack,
+    primary = CaffeBrownLight,
+    primaryVariant = CaffeBrown,
+    secondary = CaffeDarkBrown
 )
 
 @Composable
