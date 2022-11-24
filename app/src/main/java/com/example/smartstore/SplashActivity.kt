1da2745b3f19f41a091665a349834d9d2b8b1a62
김면길 <km4213@naver.com>
2022-11-24 10:05:39 +0900

    splash 페이지 추가 / 로그아웃 로직 변경

---
+++ app/src/main/java/com/example/smartstore/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.smartstore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+        val user = ApplicationClass.sharedPreferencesUtil.getUser()
+        if(user.id.isEmpty()){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
